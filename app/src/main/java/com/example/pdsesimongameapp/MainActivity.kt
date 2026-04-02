542a376d1a0798bcd2d3a9ff4734ef2a79694790
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-04-02 17:05:24 +0200

    aggiunto file xml per orientamento modalità landscape con aggiunta sottocartella a resource (layout-land) che contiene copia del file activity_main.xml, con modifiche a constraint e pesi per layout orizzontale. aggiunto codice per inizializzare pulsanti in MainActivity.kt e codice base per riceve input da pulsanti della griglia e dell'area pulsanti

--- app/src/main/java/com/example/pdsesimongameapp/MainActivity.kt
+++ app/src/main/java/com/example/pdsesimongameapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.pdsesimongameapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //devo controllare quando l'input della griglia mi serve (perche l'utente deve inserire la sequenza)
+    //o non mi serve perchè devo mostrare la sequenza all'utente o perché sono in fine partita
+    var isInputAbilitato = false
+    var stringaInput = ""
+
+    fun puoInserire() : Boolean {
+       return isInputAbilitato
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,8 +28,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val cancellaB = findViewById<Button>(R.id.cancellaB)
+        val finePartitaB = findViewById<Button>(R.id.finePartitaB)
+        val outputTV = findViewById<TextView>(R.id.outputTV)
 
+        val pulsantiInput = listOf(
+            findViewById<Button>(R.id.redB),
+            findViewById<Button>(R.id.blueB),
+            findViewById<Button>(R.id.greenB),
+            findViewById<Button>(R.id.yellowB),
+            findViewById<Button>(R.id.magentaB),
+            findViewById<Button>(R.id.cyanB)
+        )
 
+        cancellaB.setOnClickListener {
+            //contenuto area di testo si azzera
+            //sequenza in corso azzerata? intende la sequenza inserita
+            outputTV.text = ""
+            stringaInput = ""
+        }
+
+        finePartitaB.setOnClickListener {
+            isInputAbilitato = false
+            //chiamata a seconda schermata
+        }
+
+        pulsantiInput.forEach { button -> button.setOnClickListener {
+            //ogni bottone della griglia (se siamo in fase di inserimento sequenza)
+            //deve prendere il primo carattere della stringa contenuta nel suo text e inserirla nella stringa di input
+            //e nel valore text di outputTV
+            val stringaB = button.text.toString()
+            if (puoInserire()){
+                stringaInput += stringaB[0]
+                outputTV.text = stringaInput
+            }
+        } }
+
+        //solo per debug adesso
+        isInputAbilitato = true
 
     }
 }
