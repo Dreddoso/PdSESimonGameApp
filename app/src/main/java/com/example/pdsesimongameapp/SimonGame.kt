e4aa13294c164d3657fcf68d3c3fa3e3830052fb
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-04-11 00:53:24 +0200

    manca solo il cervello del gioco, l'interfaccia secondo le direttive dovrebbe essere apposto (manca migliorare estetica)

--- app/src/main/java/com/example/pdsesimongameapp/SimonGame.kt
+++ app/src/main/java/com/example/pdsesimongameapp/SimonGame.kt
@@ -4,8 +4,12 @@ package com.example.pdsesimongameapp
 //Come funziona una partita?
 //Viene data una sequenza
 
+//Il dispositivo crea una serie di toni e luci e richiede all'utente di ripetere la sequenza.
+//Se l'utente ha successo, la serie diventa progressivamente più lunga e complessa.
+//Quando l'utente fallisce o il limite di tempo scade, il gioco finisce.
+
 class SimonGame {
-    enum class ColoreSimon {
+    enum class SimonColor {
         red, green, yellow, cyan, magenta, blue
     }
 
