6fdebdc8da783bb7f6fcb97ca60c1b232af14cf7
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-14 15:55:48 +0200

    aggiunto layout land per DettaglioPartita.kt, e tolto limiti tipo maxlines o ellipsed per far si che si veda tutta la sequenza, mantenuto la struttura del elemento della lista (LinearLayout con due text view una affianco all'altra

--- app/src/main/java/com/example/pdsesimongameapp/DettaglioPartita.kt
+++ app/src/main/java/com/example/pdsesimongameapp/DettaglioPartita.kt
@@ -1,12 +1,15 @@
 package com.example.pdsesimongameapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DettaglioPartita : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +22,10 @@ class DettaglioPartita : AppCompatActivity() {
 
         val contatore = intent.getIntExtra("contatore",0)
         val sequenza = intent.getStringExtra("sequenza")
+
+        val contatoreTV : TextView = findViewById(R.id.dettagliContatoreTV)
+        contatoreTV.text = contatore.toString()
+        val sequenzaTV : TextView = findViewById(R.id.dettagliSequenzaTV)
+        sequenzaTV.text = sequenza
     }
 }
