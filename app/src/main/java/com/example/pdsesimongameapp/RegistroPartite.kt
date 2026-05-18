11c9ffb3c42c3493f8a5c0e4eb9cd6a45c806f81
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-18 11:52:04 +0200

    tolto contatore inutile

--- app/src/main/java/com/example/pdsesimongameapp/RegistroPartite.kt
+++ app/src/main/java/com/example/pdsesimongameapp/RegistroPartite.kt
@@ -1,12 +1,10 @@
 package com.example.pdsesimongameapp
 
 object RegistroPartite {
-    var countPartite : Int = 0
     val listaPartite = mutableListOf<SimonGame.SimonGameData>()
     //Partita = ( countRect, sequenza) es = ( 3, RGB)
     fun addPartita (partita: SimonGame.SimonGameData)  {
         listaPartite.add(partita)
-        countPartite++
     }
 
 }
