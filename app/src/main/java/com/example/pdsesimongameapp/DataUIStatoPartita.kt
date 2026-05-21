026db3c2a80e0890d9d701dfd1cdb6a08a6939f2
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-21 16:42:46 +0200

    tolte variabili non necessarie e rindondanti (lo stato sostituisce i boolean)

--- app/src/main/java/com/example/pdsesimongameapp/DataUIStatoPartita.kt
+++ app/src/main/java/com/example/pdsesimongameapp/DataUIStatoPartita.kt
@@ -1,14 +1,11 @@
 package com.example.pdsesimongameapp
 
-data class DataUIStatoPartita (
+data class DataUIStatoPartita(
     val statoPartita: StatoPartita = StatoPartita.IDLE,
-    val sequenzaComputer : String = "",
+    val sequenzaComputer: String = "",
     val difficoltaPartita: Int = 1,
-    val stringaInput : String = "",
-    val indiceInput : Int = 0,
-    val indiceErrore : Int = 0,
-    val indiceRiproduzione : Int = 0,
-    val isPausa : Boolean = false,
-    val isGameOver : Boolean = false,
-    val bestScore : Int = 0
+    val stringaInput: String = "",
+    val indiceInput: Int = 0,
+    val indiceErrore: Int = 0,
+    val bestScore: Int = 0
 )
