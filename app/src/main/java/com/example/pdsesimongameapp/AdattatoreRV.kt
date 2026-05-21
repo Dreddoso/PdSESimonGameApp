f73a0f93bfa62f1bdad01358822d1fe379912932
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-22 01:18:45 +0200

    modificato per funzionare con data class Partita

--- app/src/main/java/com/example/pdsesimongameapp/AdattatoreRV.kt
+++ app/src/main/java/com/example/pdsesimongameapp/AdattatoreRV.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 
-class AdattatoreRV(private val lista: List<SimonGame.SimonGameData>) : RecyclerView.Adapter<AdattatoreRV.PartitaViewHolder>() {
+class AdattatoreRV(private val lista: List<Partita>) : RecyclerView.Adapter<AdattatoreRV.PartitaViewHolder>() {
 
     fun aggiornaLista(){
         if (lista.isNotEmpty()){
@@ -21,10 +21,10 @@ class AdattatoreRV(private val lista: List<SimonGame.SimonGameData>) : RecyclerV
 
         private val contatoreTV : TextView = view.findViewById(R.id.contatoreTV)
         private val sequenzaTV : TextView = view.findViewById(R.id.sequenzaInputTV)
-                        fun bind(partita: SimonGame.SimonGameData){
-                            contatoreTV.text = partita.maxLunghezzaSequenzaCorretta.toString()
+                        fun bind(partita: Partita){
+                            contatoreTV.text = partita.score.toString()
 
-                            sequenzaTV.text = TextUtilis.getEditString(partita.sequenza,partita.indexFirstWrongChar)
+                            sequenzaTV.text = TextUtilis.getEditString(partita.sequenza,partita.indiceErrore)
                         }
     }
 
@@ -43,8 +43,8 @@ class AdattatoreRV(private val lista: List<SimonGame.SimonGameData>) : RecyclerV
             val intent = Intent(context, DettaglioPartita::class.java)
 
             intent.putExtra("sequenza", partita.sequenza)
-            intent.putExtra("bestscore",partita.maxLunghezzaSequenzaCorretta)
-            intent.putExtra("indice",partita.indexFirstWrongChar)
+            intent.putExtra("bestscore",partita.score)
+            intent.putExtra("indice",partita.indiceErrore)
             context.startActivity(intent)
         }
     }
