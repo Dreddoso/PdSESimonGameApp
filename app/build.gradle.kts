b2c96160c779916ef52d9e55ab4390be9a35b094
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-21 23:36:18 +0200

    aggiunta dependecies room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.pdsesimongameapp"
         minSdk = 26
+        //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -40,6 +41,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.room.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
