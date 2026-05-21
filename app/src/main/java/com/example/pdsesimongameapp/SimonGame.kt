e1095c317cc1526ade842d3f2ed19e80230d5fc2
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-22 01:15:33 +0200

    elimina classe data SImongameData e inizio refactoring per utilizzo database

--- app/src/main/java/com/example/pdsesimongameapp/SimonGame.kt
+++ app/src/main/java/com/example/pdsesimongameapp/SimonGame.kt
@@ -2,11 +2,6 @@ package com.example.pdsesimongameapp
 
 class SimonGame(private val caratteri : List<Char> = listOf('Y','R','G','B','M','C')) {
 
-     data class SimonGameData(
-        val maxLunghezzaSequenzaCorretta : Int = 0,
-        val sequenza : String = "",
-        val indexFirstWrongChar : Int = 0
-    )
 
     fun generaCarattere(): Char {
         return caratteri.random()
