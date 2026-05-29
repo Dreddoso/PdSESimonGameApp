f383bf7092634abfbd33fb1195034c07b798fb0a
Dreddoso <169142985+Dreddoso@users.noreply.github.com>
2026-05-29 14:12:09 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
-    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
